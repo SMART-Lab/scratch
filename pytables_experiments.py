fc9e33b0e34c1d813bd268bbf5ee65ccbe6229e5
Mathieu Germain <mathieu.germain2@usherbrooke.ca>
2013-09-17 20:47:53 -0400

    First draft of pytables read test

--- pytables_experiments.py
+++ pytables_experiments.py
@@ -8,54 +8,62 @@ rng = np.random.RandomState(1234)
 nb_examples = 70000
 complibs = ['blosc', 'lzo', 'zlib', 'bzip2']
 
-def zero_group_one_element(complib, nb_examples, f, raw, create):
-        ##### One Matrix for all input
+
+def zero_group_one_element(nb_examples, f, raw, create):
+        # One Matrix for all input
         create(raw, 'input', obj=rng.randint(0, 2, (nb_examples, 28, 28)).astype(np.bool))
         create(raw, 'output', obj=rng.randint(0, 10, nb_examples).astype(np.uint8))
 
 
-def one_group_multiple_element(complib, nb_examples, f, raw, create):
-        ##### One group with multiple elements
+def one_group_multiple_element(nb_examples, f, raw, create):
+        # One group with multiple elements
         input_data = f.create_group(raw, 'input')
         for i in range(nb_examples):
-            create(input_data, 'm_{0}_{1}'.format(i, 0), obj=rng.randint(0, 2, (28,28)).astype(np.bool))
+            create(input_data, 'm_{0}_{1}'.format(i, 0), obj=rng.randint(0, 2, (28, 28)).astype(np.bool))
 
         output_data = f.create_group(raw, 'output')
         for i in range(nb_examples):
             create(output_data, 't_{0}_{1}'.format(i, 0), obj=rng.randint(0, 10, 1))
 
 
-def mutiple_group_one_element(complib, nb_examples, f, raw, create):
-        ##### One group per example
+def mutiple_group_one_element(nb_examples, f, raw, create):
+        # One group per example
         input_data = f.create_group(raw, 'input')
         for i in range(nb_examples):
             example_i = f.create_group(input_data, 'example{0}'.format(i))
-            create(example_i, 'm_{0}_{1}'.format(i, 0), obj=rng.randint(0, 2, (28,28)).astype(np.bool))
+            create(example_i, 'm_{0}_{1}'.format(i, 0), obj=rng.randint(0, 2, (28, 28)).astype(np.bool))
 
         output_data = f.create_group(raw, 'output')
         for i in range(nb_examples):
             target_i = f.create_group(output_data, 'target{0}'.format(i))
             create(target_i, 't_{0}_{1}'.format(i, 0), obj=rng.randint(0, 10, 1))
 
-def write_file(write_method):
+
+def _write_file(write_method, f, filename, create, complib):
+    raw = f.create_group('/', 'raw')
+    start_time = t.time()
+    write_method(nb_examples, f, raw, create)
+    print "\tCompLib: {0: <3}\tTime: {1:.2f}sec\tSize: {2:.2f}Mb".format(complib, t.time() - start_time, os.path.getsize(filename) / 1024 / 1024)
+
+def write_file(write_method, dataset_name):
+    print write_method.__name__
+    #filename = "{0}_{1}_{2}.h5".format(write_method.__name__, dataset_name)
     for complib in complibs:
         my_filters = tables.filters.Filters(complevel=9, complib=complib)
-        filename = write_method.__name__+'_mnist_'+ complib +'_c9.h5'
+        filename = write_method.__name__ + '_' + dataset_name + '_' + complib + '_c9.h5'
         with tables.open_file(filename, mode='w', filters=my_filters, max_group_width=nb_examples) as f:
-            raw = f.create_group('/', 'raw')
-            start_time = t.time()
-            write_method(complib, nb_examples, f, raw, f.create_carray)
-            print "Name:{0}\tTime:{1:.4f} sec\tSize:{2:.4f} Mb".format(filename, t.time() - start_time, os.path.getsize(filename)/1024/1024)
+            _write_file(write_method, f, filename, f.create_carray, complib)
+        os.remove(filename)
 
-    filename = write_method.__name__+'_mnist_c0.h5'
+    filename = write_method.__name__ + '_' + dataset_name + '.h5'
     with tables.open_file(filename, mode='w', max_group_width=nb_examples) as f:
-        raw = f.create_group('/', 'raw')
-        start_time = t.time()
-        write_method(complib, nb_examples, f, raw, f.create_array)
-        print "Name:{0}\tTime:{1:.4f} sec\tSize:{2:.4f} Mb".format(filename, t.time() - start_time, os.path.getsize(filename)/1024/1024)
+        _write_file(write_method, f, filename, f.create_array, "None")
+    os.remove(filename)
 
-##### MAIN #####
+# MAIN #####
 if __name__ == "__main__":
-    write_file(one_group_multiple_element)
-    write_file(mutiple_group_one_element)
-    write_file(zero_group_one_element)
+    dataset_name = "mnist"
+    print "Dataset like " + dataset_name
+    write_file(one_group_multiple_element, dataset_name)
+    write_file(mutiple_group_one_element, dataset_name)
+    write_file(zero_group_one_element, dataset_name)
---
+++ pytables_read_test.py
@@ -0,0 +1,100 @@
+from __future__ import division
+import tables
+import time as t
+import numpy as np
+import os
+
+from glob import glob
+
+rng = np.random.RandomState(1234)
+nb_examples = 70000
+
+def _write_file(write_method, f, filename, create, complib):
+    raw = f.create_group('/', 'raw')
+    start_time = t.time()
+    write_method(nb_examples, f, raw, create)
+    print "\tCompLib: {0: <3}\tTime: {1:.2f}sec\tSize: {2:.2f}Mb".format(complib, t.time() - start_time, os.path.getsize(filename) / 1024 / 1024)
+
+def read_file(write_method, dataset_name):
+    print write_method.__name__
+    #filename = "{0}_{1}_{2}.h5".format(write_method.__name__, dataset_name)
+    for complib in complibs:
+        my_filters = tables.filters.Filters(complevel=9, complib=complib)
+        filename = write_method.__name__ + '_' + dataset_name + '_' + complib + '_c9.h5'
+        with tables.open_file(filename, mode='w', filters=my_filters, max_group_width=nb_examples) as f:
+            _write_file(write_method, f, filename, f.create_carray, complib)
+        os.remove(filename)
+
+    filename = write_method.__name__ + '_' + dataset_name + '.h5'
+    with tables.open_file(filename, mode='w', max_group_width=nb_examples) as f:
+        _write_file(write_method, f, filename, f.create_array, "None")
+    os.remove(filename)
+
+# MAIN #####
+if __name__ == "__main__":
+    size = 70000
+    index = range(size)
+    rng.shuffle(index)
+
+    # filename = "F_mutiple_group_one_element_mnist.h5"
+    # print filename
+    # with tables.open_file(filename, mode='r') as f:
+    #     start_time = t.time()
+    #     for i in index:
+    #         inp = f.get_node(f.root.raw.input, "example{0}".format(i))
+    #         out = f.get_node(f.root.raw.output, "target{0}".format(i))
+    #     print "Time: {0:.2f}sec Random Read".format(t.time() - start_time)
+
+    #     start_time = t.time()
+    #     inp = f.list_nodes(f.root.raw.input)
+    #     out = f.list_nodes(f.root.raw.output)
+    #     print "Time: {0:.2f}sec Seq Read".format((t.time() - start_time))
+
+    #     start_time = t.time()
+    #     for i in f.iter_nodes(f.root.raw.input):
+    #         inp = i
+    #     for i in f.iter_nodes(f.root.raw.output):
+    #         out = i
+    #     print "Time: {0:.2f}sec SeqIter Read".format((t.time() - start_time))
+
+
+    filename = "F_zero_group_one_element_mnist.h5"
+    print filename
+    import numpy as np
+    import sys
+    with tables.open_file(filename, mode='r') as f:
+        start_time = t.time()
+        #for i in range(size):
+        inp = np.asarray(f.root.raw.input)
+        out = np.asarray(f.root.raw.output)
+        print "Time: {0:.2f}sec\tSize: {1}\tSeq Read".format(t.time() - start_time, inp.nbytes + out.nbytes)
+
+        inp = []
+        out = []
+        start_time = t.time()
+        for i in index:
+            inp.append(f.root.raw.input[i])
+            out.append(f.root.raw.output[i])
+        print "Time: {0:.2f}sec\tSize: {1}\tRandom Read".format(t.time() - start_time, sys.getsizeof(inp) + sys.getsizeof(out))
+
+
+    # filename = "F_one_group_multiple_element_mnist.h5"
+    # print filename
+    # with tables.open_file(filename, mode='r') as f:
+    #     start_time = t.time()
+    #     for i in index:
+    #         inp = f.get_node(f.root.raw.input, "m_{0}_0".format(i))[0]
+    #         out = f.get_node(f.root.raw.output, "t_{0}_0".format(i))[0]
+    #     print "Time: {0:.2f}sec Random Read".format(t.time() - start_time)
+
+    #     start_time = t.time()
+    #     inp = f.list_nodes(f.root.raw.input)
+    #     out = f.list_nodes(f.root.raw.output)
+    #     print "Time: {0:.2f}sec Seq Read".format((t.time() - start_time))
+
+    #     start_time = t.time()
+    #     for i in f.iter_nodes(f.root.raw.input):
+    #         inp = i
+    #     for i in f.iter_nodes(f.root.raw.output):
+    #         out = i
+    #     print "Time: {0:.2f}sec SeqIter Read".format((t.time() - start_time))
